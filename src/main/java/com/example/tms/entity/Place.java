367471288e95602970c21b36c8046e342b486c57
hsheo <gjgkrtls@gmail.com>
2021-04-28 11:27:01 +0900

    place, Sensor_Info entity 수정

--- src/main/java/com/example/tms/entity/Place.java
+++ src/main/java/com/example/tms/entity/Place.java
@@ -1,10 +1,12 @@
 package com.example.tms.entity;
 
+import lombok.Builder;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+import java.util.Date;
 import java.util.List;
+
 @Document(collection = "place")
 public class Place {
 
@@ -12,7 +14,11 @@ public class Place {
     private ObjectId _id;
     private String name;
     private String group;
+    private String location;
+    private String admin;
+    private String tel;
     private String power;
+    private Date up_time;
     private List sensor;
 
     public ObjectId get_id() {
@@ -39,12 +45,44 @@ public class Place {
         this.group = group;
     }
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(String admin) {
+        this.admin = admin;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
     public String getPower() {
         return power;
     }
 
     public void setPower(String power) {
         this.power = power;
+    }
+
+    public Date getUp_time() {
+        return up_time;
+    }
+
+    public void setUp_time(Date up_time) {
+        this.up_time = up_time;
     }
 
     public List getSensor() {
@@ -61,8 +99,23 @@ public class Place {
                 "_id=" + _id +
                 ", name='" + name + '\'' +
                 ", group='" + group + '\'' +
+                ", location='" + location + '\'' +
+                ", admin='" + admin + '\'' +
+                ", tel='" + tel + '\'' +
                 ", power='" + power + '\'' +
+                ", up_time=" + up_time +
                 ", sensor=" + sensor +
                 '}';
+    }
+    @Builder
+    public Place(String name, String group, String location,String admin,String tel,String power,Date up_time, List sensor){
+        this.name = name;
+        this.group = group;
+        this.location = location;
+        this.admin = admin;
+        this.tel = tel;
+        this.up_time = up_time;
+        this.sensor = sensor;
+
     }
 }
--- src/main/java/com/example/tms/entity/Sensor_Info.java
+++ src/main/java/com/example/tms/entity/Sensor_Info.java
@@ -1,8 +1,11 @@
 package com.example.tms.entity;
 
+import lombok.Builder;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 
 @Document(collection = "sensor_info")
 public class Sensor_Info {
@@ -11,18 +14,11 @@ public class Sensor_Info {
     private ObjectId _id;
     private String name;
     private String naming;
-    private float warning;
-    private float danger;
-    private float substituion; //교정값
-    private int division; //구분 (1: 주요 2: 일반 3: 기타)
+    private Double legal_standard;//법적기준
+    private Double company_standard;//사내기준
+    private Double management_standard; //관리기준
+    private String power; //모니터링
 
-    public int getDivision() {
-        return division;
-    }
-
-    public void setDivision(int division) {
-        this.division = division;
-    }
 
     public ObjectId get_id() {
         return _id;
@@ -48,28 +44,36 @@ public class Sensor_Info {
         this.naming = naming;
     }
 
-    public float getWarning() {
-        return warning;
+    public Double getLegal_standard() {
+        return legal_standard;
     }
 
-    public void setWarning(float warning) {
-        this.warning = warning;
+    public void setLegal_standard(Double legal_standard) {
+        this.legal_standard = legal_standard;
     }
 
-    public float getDanger() {
-        return danger;
+    public Double getCompany_standard() {
+        return company_standard;
     }
 
-    public void setDanger(float danger) {
-        this.danger = danger;
+    public void setCompany_standard(Double company_standard) {
+        this.company_standard = company_standard;
     }
 
-    public float getSubstituion() {
-        return substituion;
+    public Double getManagement_standard() {
+        return management_standard;
     }
 
-    public void setSubstituion(float substituion) {
-        this.substituion = substituion;
+    public void setManagement_standard(Double management_standard) {
+        this.management_standard = management_standard;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        this.power = power;
     }
 
     @Override
@@ -78,9 +82,21 @@ public class Sensor_Info {
                 "_id=" + _id +
                 ", name='" + name + '\'' +
                 ", naming='" + naming + '\'' +
-                ", warning=" + warning +
-                ", danger=" + danger +
-                ", substituion=" + substituion +
+                ", legal_standard=" + legal_standard +
+                ", company_standard=" + company_standard +
+                ", management_standard=" + management_standard +
+                ", power='" + power + '\'' +
                 '}';
     }
+    @Builder
+    public Sensor_Info(String name, String naming, Double legal_standard, Double company_standard, Double management_standard, String power){
+        this.name = name;
+        this.naming = naming;
+        this.legal_standard = legal_standard;
+        this.company_standard = company_standard;
+        this.management_standard = management_standard;
+        this.power = power;
+
+    }
+
 }
