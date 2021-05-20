b40ea9b025d59990a7eb7718ae3b5636703cace6
hsheo <gjgkrtls@gmail.com>
2021-05-20 11:34:54 +0900

    MainController에서 AjaxController로 ajax 이동

--- src/main/java/com/example/tms/controller/MainController.java
+++ src/main/java/com/example/tms/controller/MainController.java
@@ -22,7 +22,7 @@ public class MainController {
     final MemberRepository memberRepository;
     final EmissionsSettingRepository emissionsSettingRepository;
     final AnnualEmissionsRepository annualEmissionsRepository;
-    final RankManagementRepository rank_managementRepository;
+    final RankManagementRepository rankManagementRepository;
     final EmissionsStandardSettingRepository emissionsStandardSettingRepository;
     final SensorListRepository sensorListRepository;
     final MongoQuary mongoQuary;
@@ -32,12 +32,12 @@ public class MainController {
     final MemberService memberService;
     final PasswordEncoder passwordEncoder;
 
-    public MainController(PlaceRepository placeRepository, MemberRepository memberRepository, EmissionsSettingRepository emissionsSettingRepository, AnnualEmissionsRepository annualEmissionsRepository, RankManagementRepository rank_managementRepository, EmissionsStandardSettingRepository emissionsStandardSettingRepository, SensorListRepository sensorListRepository, MongoQuary mongoQuary, LogRepository logRepository, EmissionsTransitionRepository emissionsTransitionRepository, RankManagementService rankManagementService, MemberService memberService, PasswordEncoder passwordEncoder) {
+    public MainController(PlaceRepository placeRepository, MemberRepository memberRepository, EmissionsSettingRepository emissionsSettingRepository, AnnualEmissionsRepository annualEmissionsRepository, RankManagementRepository rankManagementRepository, EmissionsStandardSettingRepository emissionsStandardSettingRepository, SensorListRepository sensorListRepository, MongoQuary mongoQuary, LogRepository logRepository, EmissionsTransitionRepository emissionsTransitionRepository, RankManagementService rankManagementService, MemberService memberService, PasswordEncoder passwordEncoder) {
         this.placeRepository = placeRepository;
         this.memberRepository = memberRepository;
         this.emissionsSettingRepository = emissionsSettingRepository;
         this.annualEmissionsRepository = annualEmissionsRepository;
-        this.rank_managementRepository = rank_managementRepository;
+        this.rankManagementRepository = rankManagementRepository;
         this.emissionsStandardSettingRepository = emissionsStandardSettingRepository;
         this.sensorListRepository = sensorListRepository;
         this.mongoQuary = mongoQuary;
@@ -192,7 +192,7 @@ public class MainController {
     @RequestMapping("/setting")
     public String setting(Model model,Principal principal) {
         List<Member> members = memberRepository.findAll();
-        List<RankManagement> rank_managements = rank_managementRepository.findAll();
+        List<RankManagement> rank_managements = rankManagementRepository.findAll();
         Member member = memberRepository.findById(principal.getName());
         model.addAttribute("member", member);
         model.addAttribute("members", members);
@@ -214,163 +214,7 @@ public class MainController {
     public String memberJoinGet() {
         return "memberJoin";
     }
-    /**
-     * 로그인시 입력받은 사용자 정보를 검사
-     * @param member 입력받은 사용자정보 객체
-     * @param response 뷰로 문자열을 전달하기위한 변수
-     * @throws Exception 예외처리
-     */
 
-    @RequestMapping(value = "/loginCheck", method = RequestMethod.POST)
-    public void loginCheck( Member member, HttpServletResponse response) throws Exception {
-        PrintWriter out = response.getWriter();
-        if(!memberRepository.existsById(member.getId())){ // ID가 존재하지않으면
-            out.print("id");
-        } else if(!passwordEncoder.matches(member.getPassword(),memberRepository.findById(member.getId()).getPassword())){ // password가 틀리면
-            out.print("password");
-        } else  if (memberRepository.findById(member.getId()).getState().equals("5")){ // 가입거절
-            out.print("denie");
-        } else  if (memberRepository.findById(member.getId()).getState().equals("4")){ // 가입대기
-            out.print("waiting");
-        }else {
-            Member newMember = memberRepository.findById(member.getId());
-            Date time = new Date();
-            newMember.setLastLogin(time);
-            memberRepository.save(newMember);
-        }
-    }           // loginCheck
-    /**
-     * 회원가입신청 한 유저의 승낙 여부를 결정
-     * @param id 가입신청한유저의 id
-     * @param iNumber 0 - 거부 / 1 - 승인 을나타냄
-     * @return 안내메시지를 리턴
-     */
-    @RequestMapping(value = "/signUp", method = RequestMethod.POST)
-    public @ResponseBody String memberSignUp(String id, String iNumber){
-        String msg = "";
-        Member newMember = memberRepository.findById(id);
-        if(iNumber.equals("0")){
-            newMember.setState("5");
-            msg = "가입 거절 되었습니다.";
-        }else{
-            newMember.setState("3"); // 5: 거절 - 4: 가입대기 - 3: 일반 - 2: 관리자 - 1: 최고관리자
-            Date time = new Date();
-            newMember.setJoined(time); // 가입승인일 설정
-            msg = "가입 승인 되었습니다.";
-        }
-        memberRepository.save(newMember);
-        return msg;
-    }           // memberSignUp
-    /**
-     * 입력받은 값으로 유저의 등급을 결정
-     * @param id 등급을 결정할 유저의 id
-     * @param value 등급값
-     * @return 안내메시지 리턴
-     */
-    @RequestMapping(value = "/gaveRank", method = RequestMethod.POST)
-    public @ResponseBody String gaveRank(String id, String value){
-        Member newMember = memberRepository.findById(id);
-        newMember.setState(value);
-        memberRepository.save(newMember);
-        return "등급 부여 하였습니다.";
-    }           // gaveRank
-    /**
-     * 입력받은 값으로 유저의 비밀번호를 초기화
-     * @param id 초기화할 유저의 id
-     * @return 안내메시지와 임시비밀번호 메시지 리턴
-     */
-    @RequestMapping(value = "/resetPassword", method = RequestMethod.POST)
-    public @ResponseBody String resetPassword(String id){
-        Member newMember = memberRepository.findById(id);
-        String uuid = (UUID.randomUUID().toString().replaceAll("-", "")).substring(0,10);
-        String encodedPwd = passwordEncoder.encode(uuid);
-        newMember.setPassword(encodedPwd);
-        memberRepository.save(newMember);
-        return "비밀번호가 초기화 되었습니다. \n임시비밀번호 : "+ uuid;
-    }           // resetPassword
-    /**
-     * 입력받은 값으로 유저를 추방
-     * @param id 추방할 유저의 id
-     * @return 안내메시지 리턴
-     */
-    @RequestMapping(value = "/kickMember", method = RequestMethod.POST)
-    public @ResponseBody String kickMember(String id){
-        memberRepository.deleteById(id);
-        return "추방처리 되었습니다.";
-    }           // kickMember
-    /**
-     * 변경한 권한관리 값들을 저장
-     * @param rankManagement 변경한 값들을 담고있는 객체
-     * @param response 뷰로문자열을 전달하기위한 변수
-     * @throws Exception 예외처리
-     */
-    @RequestMapping(value = "/rankSettingSave", method = RequestMethod.POST)
-    @ResponseBody
-    public void rankSettingSave(@RequestBody RankManagement rankManagement, HttpServletResponse response) throws Exception {
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        PrintWriter out = response.getWriter();
-
-        RankManagement newRankManagement = rank_managementRepository.findByName(rankManagement.getName());
-
-        String str = "";
-        str += (rankManagement.isDashboard() == newRankManagement.isDashboard()) ? "" : (rankManagement.isDashboard()) ? "대시보드 메뉴열람 권한부여  " : "대시보드 메뉴열람 권한해제  " ;
-        str += (rankManagement.isAlarm() == newRankManagement.isAlarm()) ? "" : (rankManagement.isAlarm()) ? "알림 메뉴열람 권한부여  " : "알림 메뉴열람 권한해제  " ;
-        str += (rankManagement.isMonitoring() == newRankManagement.isMonitoring()) ? "" : (rankManagement.isMonitoring()) ? "모니터링 메뉴열람 권한부여  " : "모니터링 메뉴열람 권한해제  " ;
-        str += (rankManagement.isStatistics() == newRankManagement.isStatistics()) ? "" : (rankManagement.isStatistics()) ? "분석및통계 메뉴열람 권한부여  " : "분석및통계 메뉴열람 권한해제  " ;
-        str += (rankManagement.isSetting() == newRankManagement.isSetting()) ? "" : (rankManagement.isSetting()) ? "환경설정 메뉴열람 권한부여" : "환경설정 메뉴열람 권한해제" ;
-        out.print(str);
-
-        newRankManagement.setDashboard(rankManagement.isDashboard());
-        newRankManagement.setAlarm(rankManagement.isAlarm());
-        newRankManagement.setMonitoring(rankManagement.isMonitoring());
-        newRankManagement.setStatistics(rankManagement.isStatistics());
-        newRankManagement.setSetting(rankManagement.isSetting());
-        rank_managementRepository.save(newRankManagement);
-    }           // rankSettingSave
-    /**
-     * 로그정보를 날짜추가후 DB에 저장
-     * @param log Log정보
-     */
-    @RequestMapping(value = "/inputLog", method = RequestMethod.POST)
-    @ResponseBody
-    public void inputLog(@RequestBody Log log){
-        log.setDate(new Date());
-        logRepository.save(log);
-    }           // inputLog
-    /**
-     * 현재 로그인한 유저의 이름을 문자로 리턴함
-     * @param principal 로그인유저의 정보객체
-     * @return 유저의 이름문자 리턴
-     */
-    @RequestMapping(value = "/getUsername", method = RequestMethod.POST)
-    @ResponseBody
-    public String getUsername(Principal principal) {
-        Member member = memberRepository.findById(principal.getName());
-        return member.getName();
-    }           // getUsername
-    /**
-     * 입력받은 값을 바탕으로 DB에 저장되어있는 권한값을 리턴함
-     * @param principal
-     * @return
-     */
-    @RequestMapping(value = "/getRank", method = RequestMethod.POST)
-    @ResponseBody
-    public RankManagement getRank(Principal principal) {
-        Member member = memberRepository.findById(principal.getName());
-        String state = member.getState();
-        String str;
-        if(state.equals("3")){
-            str = "normal";
-        } else if (state.equals("2")){
-            str = "admin";
-        } else if (state.equals("1")){
-            str = "root";
-        } else {
-            str = "denie";
-        }
-        return rank_managementRepository.findByName(str);
-    }           // getRank
     /**
      * 분석 및 통계 - 측정 자료 조회
      * @param model 측정소와 센서
@@ -445,26 +289,7 @@ public class MainController {
 
         return "emissionsManagement";
     }
-    /**
-     * 배출량 모니터링 상태값을 변경
-     * @param sensor 상태값 변경할 센서
-     * @param isCollection 배출량 추이 모니터링 <->연간 배출량 누적 모니터링   판별할 데이터
-     */
-    @ResponseBody
-    @RequestMapping("emissionsState")
-    public void emissionsState(String sensor, boolean isCollection) {
-           //배출량 설정
-        if (isCollection) {
-            EmissionsSetting target = emissionsSettingRepository.findBySensor(sensor);
-            target.setStatus(!target.isStatus());
-            emissionsSettingRepository.save(target);
-            //연간 배출량 설정
-        } else {
-            AnnualEmissions target = annualEmissionsRepository.findBySensor(sensor);
-            target.setStatus(!target.isStatus());
-            annualEmissionsRepository.save(target);
-        }
-    }
+
 
 
 }
